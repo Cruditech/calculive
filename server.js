f3e1981b6a963899d501e8cb66028eee2601dd53
Erin A <erinakarice@gmail.com>
2024-01-03 23:45:10 +0000

    logger no logging

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ const config = {
 const port = process.env.PORT || 3000;
 if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
   config.baseURL = `http://localhost:${port}`;
+} else {
+  config.baseURL = process.env.BASE_URL;
 }
 
 app.use(auth(config));
